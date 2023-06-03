5f7102112daf929c2568a3ddec09ff119ce7f6cf
Yash Kumar <90888884+yashk9293@users.noreply.github.com>
2023-06-04 03:59:31 +0530

    updated index.js

--- index.js
+++ index.js
@@ -1,21 +1,26 @@
-// const clock = document.querySelector(".time");
 let timer = null;
 
-let sec = 0,
+let msec = 0,
+  sec = 0,
   min = 0,
   hr = 0,
   intervalId;
 
 function Stopwatch() {
-    sec++;
-    if(sec==60) {
-        sec=0;
-        min++;
-        if(min==60) {
-            min=0;
-            hr++;
+    msec++;
+    if(msec == 100) {
+        msec = 0;
+        sec++;
+        if(sec==60) {
+            sec=0;
+            min++;
+            if(min==60) {
+                min=0;
+                hr++;
+            }
         }
     }
+    const milliseconds = msec.toString().padStart(2, "0");
     const seconds = sec.toString().padStart(2, "0");
     const minutes = min.toString().padStart(2, "0");
     const hours = hr.toString().padStart(2, "0");
@@ -23,13 +28,14 @@ function Stopwatch() {
     document.getElementById("hr").innerHTML = hours;
     document.getElementById("min").innerHTML = minutes;
     document.getElementById("sec").innerHTML = seconds;
+    document.getElementById("msec").innerHTML = milliseconds;
 }
 
 function startStopwatch() {
     if(timer !== null) {
         clearInterval(timer);
     }
-    timer = setInterval(Stopwatch,1000)
+    timer = setInterval(Stopwatch,10)
 }
 
 const stopStopwatch = () => {
@@ -38,8 +44,9 @@ const stopStopwatch = () => {
 
 const resetStopwatch = () => {
     clearInterval(timer);
-    sec = 0, min = 0, hr = 0;
+    msec = 0, sec = 0, min = 0, hr = 0;
 
+    const milliseconds = msec.toString().padStart(2, "0");
     const seconds = sec.toString().padStart(2, "0");
     const minutes = min.toString().padStart(2, "0");
     const hours = hr.toString().padStart(2, "0");
@@ -47,4 +54,5 @@ const resetStopwatch = () => {
     document.getElementById("hr").innerHTML = hours;
     document.getElementById("min").innerHTML = minutes;
     document.getElementById("sec").innerHTML = seconds;
+    document.getElementById("msec").innerHTML = milliseconds;
   };
